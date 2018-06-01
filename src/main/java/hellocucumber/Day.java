e3c02ba41df8d617959a16288890603f626f736d
Steve Swinsburg <steve.swinsburg@orionhealth.com>
2018-06-01 13:19:50 +1000

    Add some sample code

---
+++ src/main/java/hellocucumber/Day.java
@@ -0,0 +1,16 @@
+package hellocucumber;
+
+public class Day {
+
+    private String dayName;
+
+    public Day() {
+        // change to Calendar get day name
+        this.day = "Sunday";
+    }
+
+    public String getDayName() {
+        return this.dayName;
+    }
+
+}
